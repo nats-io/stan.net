3b0fd8c86255e99c4ceb19f878ef0bbccf662d83
Colin Sullivan <colin.sullivan@apcera.com>
2017-02-17 15:25:47 -0700

    Add .NET core support

--- nuget/build_pkg.bat
+++ nuget/build_pkg.bat
@@ -1,5 +1,5 @@
 @setlocal
-REM @echo off
+@echo off
 
 REM This builds the NATS Streaming .NET Client NuGet package, and is 
 REM intended for use by Apcera to create NuGet Packages.
@@ -12,6 +12,7 @@ if errorlevel 9009 if not errorlevel 9010 (
 
 set STAN_CLIENT_45=..\net45\STAN.Client\bin\Release\STAN.Client.DLL
 set STAN_CLIENT_XML_45=..\net45\STAN.Client\bin\Release\STAN.Client.XML
+set STAN_CORE_DIR=..\STAN.Client\bin\Release\netstandard1.6
 
 if NOT EXIST %STAN_CLIENT_45% (
     echo Cannot find %STAN_CLIENT_45%
@@ -24,15 +25,21 @@ if NOT EXIST %STAN_CLIENT_XML_45% (
     goto End
 )
 
+if NOT EXIST %STAN_CORE_DIR% (
+    echo Cannot find .NET core build.
+    goto End
+)
+
 mkdir tmp 2>NUL
 mkdir tmp\lib 2>NUL
 mkdir tmp\lib\net45 2>NUL
+mkdir tmp\lib\netstandard1.6 2>NUL
 
 copy %STAN_CLIENT_45% tmp\lib\net45 1>NUL
 copy %STAN_CLIENT_XML_45% tmp\lib\net45 1>NUL
+copy %STAN_CORE_DIR%\* tmp\lib\netstandard1.6 1>NUL
 
-
-REM (to recreate) nuget spec -f -Verbosity detailed -AssemblyPath NATS.Client.DLL
+REM (to recreate) nuget spec -f -Verbosity detailed -AssemblyPath STAN.Client.DLL
 
 cd tmp
 
